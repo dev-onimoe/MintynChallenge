45038c3a69d558f2f32ed15cf99b85bc96a530e5
devOnimoe <68595786+dev-onimoe@users.noreply.github.com>
2022-11-28 15:32:53 +0100

    second commit

---
+++ Pods/Target Support Files/Pods-MintynChallenge-MintynChallengeUITests/Pods-MintynChallenge-MintynChallengeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MintynChallenge_MintynChallengeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MintynChallenge_MintynChallengeUITests
+@end
